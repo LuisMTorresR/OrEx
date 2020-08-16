255550d822f0d7d5a1b526c11d921fdb3e9f7b61
Luis <luismtorresrequena@gmail.com>
2020-08-15 22:49:48 -0300

    Inicio del Proyecto OrEx

---
+++ src/clases/Disennio.java
@@ -0,0 +1,35 @@
+package clases;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author luism
+ */
+public class Disennio {
+    
+    private String logo = "src/images/Logo.png";
+    private String wallpaper = "src/images/Fondo.jpg";
+    
+        public Disennio() {
+    }
+
+
+    public String getWallpaper() {
+        return wallpaper;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+
+    
+
+    public Image getIconImage() {
+        Image retValue = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("images/Logo.png"));
+        return retValue;
+    }
+
+}
